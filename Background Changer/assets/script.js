061a22b6fee37539b9de12c1449af6b375a73629
tpessoa10 <48775460+tpessoa10@users.noreply.github.com>
2023-03-24 11:48:41 -0300

    Add files via upload

---
+++ Background Changer/assets/script.js
@@ -0,0 +1,15 @@
+var button = document.getElementById("button")
+var dark_theme = false
+function change_background(){
+    if (dark_theme == false){
+        dark_theme = true
+        document.body.style.background = "white"
+        button.style.background = "black"
+        button.style.color = "white"
+    }else{
+        dark_theme = false
+        document.body.style.background = "black"
+        button.style.background = "white"
+        button.style.color = "black"
+    }
+}
